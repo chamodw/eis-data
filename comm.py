d4bf5aaad6964f108dca4a39662fc278713b745c
Chamod <chamod@ahlab.org>
2021-06-06 19:05:04 +1200

    add capability to change PGA gain and output drive voltage from the script

--- comm.py
+++ comm.py
@@ -8,14 +8,34 @@ from datetime import datetime
 
 
 
-gain_factor = 1
+gain_factor = 4.9722879409529194e-09 
+
+
+v_range_names=['0', '2.0V', '1.0V', '400mV', '200mV']
+v_range_values=[0, 0, 3, 2, 1];
+
+#Generates command packet for EIS Device
+#  Returns a tuple (size, packed_byte_structure)
+
+# +- Header
+# +- length 
+# +- 0
+# +- Start Frequency
+# +- Increment frequency
+# +- Number of increments
+# +- Resistance
+# +- Averaging count 
+# +- 0
+# +- 0
+# +- Footer
+
 
 def getPacket():
 	s = struct.Struct("I I I I I I I I I I I");
 	header=0xb5280013
-	l=3
+	l=5
 	footer=0x31a39b0e
-	d=[header, l, 0,  start_freq, increment, num_increments,  0, num_avg, 0, 0, footer];
+	d=[header, l, 0,  start_freq, increment, num_increments,  0, num_avg, (v_range_values[v_range]<<8)+pga_gain, 0, footer];
 	packed_data=s.pack(*d);
 	return (s.size, packed_data)
 
@@ -29,16 +49,20 @@ def printColor(text, color):
 	
 	print (prefix+text+suffix)
 
+# Generates a fancy progress bar
+# Parameters (progress_count, full_count)
+# Returns a string
 def getProgressBar (i, n):
 	p = '='*int(i*80/n) + '-'*(80-int(i*80/n)) + '|'
 	return p
 
+# Print example usage if not enough arguments are provided 
 if (len(sys.argv) < 3):
 	printColor ("Usage comm.py port data_directory sample_id", 'r')
 	exit()
 
 port = sys.argv[1]
-path = sys.argv[2]
+path = sys.argv[2] # Datafile output path
 
 
 
@@ -51,10 +75,14 @@ except:
 	printColor("EIS Device not found", 'r')
 	exit(1)
 
-start_freq=1000
-increment=10
+start_freq=30000
+increment=200
 num_increments=100
-num_avg = 4
+num_avg = 2
+v_range = 3
+pga_gain = 1
+
+
 #
 #print ("Enter session id (solution_electrode)")
 #session_id = input()
@@ -63,6 +91,7 @@ num_avg = 4
 #print ("Enter concentrations, separated by commas")
 #s = input()
 #concentrations = [float(x) for x in s.split(',')]
+
 #
 session_id = 'avg2'
 temperature = 25
@@ -73,6 +102,8 @@ print ("Session ID: ", session_id);
 print ("Start Frequency: ", start_freq/1000, "kHz")
 print ("Frequency Increment: ", increment)
 print ("Number of Increments: ", num_increments)
+printColor("Gain factor = " + str(gain_factor), 'g')
+printColor("Voltage range " + v_range_names[v_range], 'g')
 for i_conc in range(n_conc):
 #	conc = concentrations[i_conc] #current concentration
 #	printColor(getProgressBar(i_conc+1, n_conc), 'g')
@@ -106,7 +137,6 @@ for i_conc in range(n_conc):
 
 		if (line == b''):
 			continue
-		print (line)
 		data_line = [int(x) for x in line.decode('utf-8').split(',')]
 		cur_freq = start_freq + increment*data_line[0]
 		freq.append(cur_freq)
@@ -115,15 +145,23 @@ for i_conc in range(n_conc):
 		real.append(data_line[1])
 		im.append(data_line[2])
 		log_str = ','.join([str(x) for x in data_line])
+
+		
 #		out_file.write(log_str)
 #		out_file.write("\n")
-	print (real)
+
+	print('\033[0m') # Reset terminal colors
+
+
+	mag = [np.sqrt(real[x]**2+im[x]**2) for x in range(len(real))]
+	impedance = [1/(gain_factor*m) for m in mag]
+	print ("Real range : " + str( min(real)) + " - " + str(max(real))), 	
+	print ("Imaginary range : " + str( min(im)) + " - " + str(max(im))), 	
 	plt.figure(1)
-	plt.plot(freq[1:], real[1:], label = 'Real')
-	plt.plot(freq[1:], im[1:], label = 'imag')
+	plt.plot(freq, impedance, label = 'Mag')
+	plt.legend(str(i_conc+1))
 	plt.pause(1)
-#	x = input()
-#	ax.plot(freq[1:], im[1:])
+	x = input()
 
 #	#Attempt plolar plot
 #	try:
@@ -133,8 +171,7 @@ for i_conc in range(n_conc):
 #		plt.show()
 #	except:
 #		printColor("Polar plot failed", 'r')
-#
-#
+
 	continue
 	out_file.close()
 	printColor("\nData writeen to " + out_file.name, 'g')
